3586a54d3babfca07b5816327c1ea7a5fa861c80
orianac <orianac@uw.edu>
2020-01-23 13:17:53 -0800

    adding some pseudocode for reading in the sample data

--- futurewater.py
+++ futurewater.py
@@ -23,3 +23,22 @@ import cftime
 # Globals
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # This line checks if GPU is available
 WATERFORAG_ROOT = Path('/pool0/data/orianac/waterforag/')
+
+def load_historical_forcing():
+    ds = 'an xarray dataset with all variables for the subbasin of interest'
+    return ds
+
+
+
+def load_hindcasted_forcing():
+    forcing_path = WATERFORAG_ROOT / 'meteorology' / 'hindcast'
+    ds = 'an xarray dataset with all forecasted variables for the subbasin of interest'
+    return ds
+    
+
+class gridmet_NMME(Dataset):
+# this class will house the datasets to be used for training and validation for a selected basin
+# it is specific to the gridmet historical dataset and the NMME climate forecasts
+  #  def __init__():
+    def __init():
+        return 'hello'
