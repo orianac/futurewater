00ab01e5b10d4b99db2dbf3fd13c839664d0f7aa
orianac <orianac@uw.edu>
2020-01-28 10:19:43 -0800

    Adding in the pieces from Freddy Kratzert's example notebook that are dataset-agnostic.

--- futurewater.py
+++ futurewater.py
@@ -62,9 +62,130 @@ class gridmet_NMME(Dataset):
         # here we'll want to subset the forcing for the specific basin we're working with
         # like this:
         # ds_subset = mask(ds, basin_mask)
-        
+        # if mean:
+            # convert to basin mean values
 
-        # add the code that converts the forcings into the 
+        # add the code that converts the forcings into the shape appropriate for pytorch
+        
         return ds
+
+class Model(nn.Module):
+    """Implementation of a single layer LSTM network. Taken directly from sample notebook"""
+    
+    def __init__(self, hidden_size: int, dropout_rate: float=0.0):
+        """Initialize model
+        
+        :param hidden_size: Number of hidden units/LSTM cells
+        :param dropout_rate: Dropout rate of the last fully connected
+            layer. Default 0.0
+        """
+        super(Model, self).__init__()
+        self.hidden_size = hidden_size
+        self.dropout_rate = dropout_rate
+        
+        # create required layer
+        self.lstm = nn.LSTM(input_size=5, hidden_size=self.hidden_size, 
+                            num_layers=1, bias=True, batch_first=True)
+        self.dropout = nn.Dropout(p=self.dropout_rate)
+        self.fc = nn.Linear(in_features=self.hidden_size, out_features=1)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass through the Network.
+        
+        :param x: Tensor of shape [batch size, seq length, num features]
+            containing the input data for the LSTM network.
+        
+        :return: Tensor containing the network predictions
+        """
+        output, (h_n, c_n) = self.lstm(x)
+        
+        # perform prediction only at the end of the input sequence
+        pred = self.fc(self.dropout(h_n[-1,:,:]))
+        return pred
+
+def train_epoch(model, optimizer, loader, loss_func, epoch):
+    """Train model for a single epoch.
+
+    :param model: A torch.nn.Module implementing the LSTM model
+    :param optimizer: One of PyTorchs optimizer classes.
+    :param loader: A PyTorch DataLoader, providing the trainings
+        data in mini batches.
+    :param loss_func: The loss function to minimize.
+    :param epoch: The current epoch (int) used for the progress bar
+    """
+    # set model to train mode (important for dropout)
+    model.train()
+    pbar = tqdm.tqdm_notebook(loader)
+    pbar.set_description(f"Epoch {epoch}")
+    # request mini-batch of data from the loader
+    for xs, ys in pbar:
+        # delete previously stored gradients from the model
+        optimizer.zero_grad()
+        # push data to GPU (if available)
+        xs, ys = xs.to(DEVICE), ys.to(DEVICE)
+        # get model predictions
+        y_hat = model(xs)
+        # calculate loss
+        loss = loss_func(y_hat, ys)
+        # calculate gradients
+        loss.backward()
+        # update the weights
+        optimizer.step()
+        # write current loss in the progress bar
+        pbar.set_postfix_str(f"Loss: {loss.item():.4f}")
+
+        
+def eval_model(model, loader) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Evaluate the model.
+
+    :param model: A torch.nn.Module implementing the LSTM model
+    :param loader: A PyTorch DataLoader, providing the data.
+    
+    :return: Two torch Tensors, containing the observations and 
+        model predictions
+    """
+    # set model to eval mode (important for dropout)
+    model.eval()
+    obs = []
+    preds = []
+    # in inference mode, we don't need to store intermediate steps for
+    # backprob
+    with torch.no_grad():
+        # request mini-batch of data from the loader
+        for xs, ys in loader:
+            # push data to GPU (if available)
+            xs = xs.to(DEVICE)
+            # get model predictions
+            y_hat = model(xs)
+            obs.append(ys)
+            preds.append(y_hat)
+            
+    return torch.cat(obs), torch.cat(preds)
+        
+def calc_nse(obs: np.array, sim: np.array) -> float:
+    """Calculate Nash-Sutcliff-Efficiency.
+
+    :param obs: Array containing the observations
+    :param sim: Array containing the simulations
+    :return: NSE value.
+    """
+    # only consider time steps, where observations are available
+    sim = np.delete(sim, np.argwhere(obs < 0), axis=0)
+    obs = np.delete(obs, np.argwhere(obs < 0), axis=0)
+
+    # check for NaNs in observations
+    sim = np.delete(sim, np.argwhere(np.isnan(obs)), axis=0)
+    obs = np.delete(obs, np.argwhere(np.isnan(obs)), axis=0)
+
+    denominator = np.sum((obs - np.mean(obs)) ** 2)
+    numerator = np.sum((sim - obs) ** 2)
+    nse_val = 1 - numerator / denominator
+
+    return nse_val
+
+
 ensemble_member = '23'
-ds_train = gridmet_NMME(ensemble_member)
+period = 'train'
+ds_train = gridmet_NMME(ensemble_member=ensemble_member, period=period)
+
+
